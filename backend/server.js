bb84138e942b8539296ab2cbefbb9ba4ac1fb942
Ahmed Hawas <ahmedhawas600@gmail.com>
2025-11-09 03:05:03 +0200

    Added DEPLOYMENT_SIMPLE.md

--- backend/server.js
+++ backend/server.js
@@ -18,4 +18,4 @@ app.use('/api/users', userRoutes)
 app.use('/api/scores', scoreRoutes)
 
 const PORT = process.env.PORT || 5000
-app.listen(PORT, ()=> console.log(`🚀 Server running on port ${PORT}`))
+app.listen(PORT, '0.0.0.0', ()=> console.log(`Server running on port ${PORT}`))
