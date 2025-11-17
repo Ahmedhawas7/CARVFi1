0d814e868b5ae42eea5401c70df1ba68ad421473
Ahmed Hawas <ahmedhawas600@gmail.com>
2025-11-17 20:39:27 +0200

    Delete backend directory

--- backend/routes/scoreRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from 'express'
-import { addScore, getScores } from '../controllers/scoreController.js'
-const router = express.Router()
-
-router.post('/add', addScore)
-router.get('/list', getScores)
-
-export default router
--- backend/routes/userRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from 'express'
-import { getOrCreateUser, getUserStats } from '../controllers/userController.js'
-const router = express.Router()
-
-router.post('/connect', getOrCreateUser)
-router.get('/stats', getUserStats)
-
-export default router
